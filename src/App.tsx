e2d4041160aaf15c76218a4b43942f42232cd296
John <emailjohnthomascaballero@gmail.com>
2024-10-18 22:01:52 +0800

    Day 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
+
+  function increment() {
+    // setCount(count + 1) // you can use this method too.
+    setCount((prevCount) => prevCount + 1)
+  }
 
   return (
-    <div>test</div>
+    <div>
+      <button onClick={increment}>Increment {count}</button>
+    </div>
   )
 }
 
