36312fd59d1d9622e053392b56be722629e219ec
John <emailjohnthomascaballero@gmail.com>
2024-10-30 19:19:36 +0800

    Day 12

--- src/games.tsx
+++ src/games.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
+export type Game = {
+  id: number;
+  name: "Dota2" | "CS:GO" | "League of Legends";
+  difficulty: "Easy" | "Medium" | "Hard";
+};
+
+const GAMES_DATA = Array.from({ length: 300 }, (_, index) => {
+  return {
+    id: index + 1,
+    name: index % 2 ? "Dota2" : "League of Legends",
+    difficulty: index % 2 ? "Easy" : "Hard",
+  } satisfies Game;
+});
+
 export default function GamesComponent() {
+  const [games, setGames] = useState(GAMES_DATA);
+
   return (
     <div>
-      Games
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>
+            {game.name} - {game.difficulty}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
