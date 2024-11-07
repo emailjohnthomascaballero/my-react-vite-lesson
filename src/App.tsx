bc13369be0a22e776d5d02c9caaf0601332acd9e
John <emailjohnthomascaballero@gmail.com>
2024-11-07 18:36:08 +0800

    Day 20

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import TestComponent from './test'
-import Todos from './todos'
-import Ref from './ref'
-import Games from './games'
+import "./App.css";
+import { Day2MainComponent } from "./day2-custom-hooks";
+// import TestComponent from './test'
+// import Todos from './todos'
+// import Ref from './ref'
+// import Games from './day1-fundamentals/games'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
-  function increment() {
-    // setCount(count + 1) // you can use this method too.
-    setCount((prevCount) => prevCount + 1) // or you can use this method too & this is recommended.
-  }
+  // function increment() {
+  //   // setCount(count + 1) // you can use this method too.
+  //   setCount((prevCount) => prevCount + 1) // or you can use this method too & this is recommended.
+  // }
 
   // every component render useEffect will run.
-  useEffect(() => {
-    console.log(`I am from app component`)
-  }, []);
+  // useEffect(() => {
+  //   console.log(`I am from app component`)
+  // }, []);
 
   return (
     <div>
       {/* <button onClick={increment}>Increment {count}</button>
       <TestComponent /> */}
       {/* <Todos /> */}
-      {/* <Ref /> */} 
-      <Games />
+      {/* <Ref /> */}
+      {/* <Games /> */}
+      {<Day2MainComponent />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
