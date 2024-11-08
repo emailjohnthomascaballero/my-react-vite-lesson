c7bd774e14103cb0174fa64aef28139c26d7bcd2
John <emailjohnthomascaballero@gmail.com>
2024-11-08 20:55:00 +0800

    Day 21

--- src/day2-custom-hooks/index.tsx
+++ src/day2-custom-hooks/index.tsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
 
 // useCounter is the custom hook
-function useCounter() {
-  const [count, setCount] = useState(0);
-  return { count, setCount };
-}
+// function useCounter() {
+//   const [count, setCount] = useState(0);
+//   return { count, setCount };
+// }
 
 export function Day2MainComponent() {
-  const { count, setCount } = useCounter();
+  // const { count, setCount } = useCounter();
 
   return (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      {/* <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button> */}
     </div>
   );
 }
