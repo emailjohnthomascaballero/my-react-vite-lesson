afcc9ccb2fdf64346f4edf50526796400dbe8e81
John <emailjohnthomascaballero@gmail.com>
2024-11-09 16:25:56 +0800

    Day 22

--- src/day2-custom-hooks/index.tsx
+++ src/day2-custom-hooks/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // useCounter is the custom hook
 // function useCounter() {
@@ -6,13 +6,39 @@ import { useState } from "react";
 //   return { count, setCount };
 // }
 
+type Post = {
+  id: number,
+  title: string,
+  body: string
+}
+
 export function Day2MainComponent() {
   // const { count, setCount } = useCounter();
+
+  const [data , setData] = useState<Post[]>([]);
+
+  const fetchData = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    setData(data);
+  }
+
+  // first render will fetch the data
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
       {/* <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button> */}
+
+      <h1>Posts</h1>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
